e5f6f580e5a2d8081a3c8ec78530ce097579d512
Mateo Tomalino <mateotomalino@gmail.com>
2024-03-06 12:09:39 -0600

    PopUp comments - movi el PopUp a un componente separado, tambien su CSS - cambie algunas variables en espanol por ingles - cambie el nombre de columba "mensaje" por "comentarios" - el setComments por default carga los comentarios ya existentes de ese item - agregue un comentario al primer item (para tener de ejemplo)

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -7,10 +7,12 @@ const ItemListContainer = ({username}) => {
     
     const [selectedItem, setSelectedItem] = useState(null);
     const items = [
-        { id: 1, name: 'Juan Cruz', fecha: '02/04/2020', patente:'vot 235' ,compañia: 'san cristobal', monto:5000, estado: 'activo'},
-        { id: 2, name: 'Mateo', fecha: '02/04/2020', patente: 'npm 123' ,compañia: 'la caja', monto:24000, estado: 'completo'},
-        { id: 3, name: 'Agostina', fecha: '02/04/2020', patente: 'abc 456',compañia: 'sancor', monto:2000 , estado: 'pendiente'},
-        { id: 3, name: 'Pepito', fecha: '03/04/2020', patente: 'abc 789',compañia: 'sancor', monto:4000 , estado: 'completo'}
+        { id: 1, name: 'Juan Cruz', date: '02/04/2020', plate:'vot 235' ,company: 'san cristobal', amount:5000, state: 'activo', 
+            comments: [{ text: 'El reclamo se encuentra en proceso', date: '02/04/2020', user: 'Juan Cruz Masjoan'}]
+        },
+        { id: 2, name: 'Mateo', date: '02/04/2020', plate: 'npm 123' ,company: 'la caja', amount:24000, state: 'completo'},
+        { id: 3, name: 'Agostina', date: '02/04/2020', plate: 'abc 456',company: 'sancor', amount:2000 , state: 'pendiente'},
+        { id: 3, name: 'Pepito', date: '03/04/2020', plate: 'abc 789',company: 'sancor', amount:4000 , state: 'completo'}
     ];
 
     const handleItemClick = item => {
--- src/components/itemList/ItemList.jsx
+++ src/components/itemList/ItemList.jsx
@@ -1,35 +1,17 @@
 import React, { useState } from 'react';
-import classes from './ItemList.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDownload, faEdit, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import CommentsPopUp from '../commentsPopUp/commentsPopUp';
+import classes from './ItemList.module.css';
 
 const ItemList = ({ items, onItemClick, username }) => {
-    const [showMessagePopup, setShowMessagePopup] = useState(false);
+    const [showMessagePopup, setShowCommentsPopUp] = useState(false);
     const [selectedItem, setSelectedItem] = useState(null);
-    const [comment, setComment] = useState('');
-    const [comments, setComments] = useState([]);
 
     const handleMessageClick = (event, item) => {
         event.stopPropagation(); 
         setSelectedItem(item);
-        setShowMessagePopup(true);
-    };
-
-    const handleCloseMessagePopup = () => {
-        setShowMessagePopup(false);
-    };
-
-    const handleCommentChange = (event) => {
-        setComment(event.target.value);
-    };
-
-    const handleCommentSubmit = (event) => {
-        event.preventDefault();
-        if (comment.trim() !== '') {
-            const newComment = { text: comment, date: new Date().toLocaleString(), user: username };
-            setComments([...comments, newComment]);
-            setComment('');
-        }
+        setShowCommentsPopUp(true);
     };
 
     return (
@@ -46,7 +28,7 @@ const ItemList = ({ items, onItemClick, username }) => {
                         <th>Estado</th>
                         <th>Descargar</th>
                         <th>Modificar</th>
-                        <th>Mensaje</th>
+                        <th>Comentarios</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -54,11 +36,11 @@ const ItemList = ({ items, onItemClick, username }) => {
                         <tr key={item.id} onClick={() => onItemClick(item)}>
                             <td>{item.id}</td>
                             <td>{item.name}</td>
-                            <td>{item.fecha}</td>
-                            <td>{item.patente}</td>
-                            <td>{item.compañia}</td>
-                            <td>{item.monto}</td>
-                            <td>{item.estado}</td>
+                            <td>{item.date}</td>
+                            <td>{item.plate}</td>
+                            <td>{item.company}</td>
+                            <td>{item.amount}</td>
+                            <td>{item.state}</td>
                             <td><FontAwesomeIcon icon={faDownload} className="action-icon" /></td>
                             <td><FontAwesomeIcon icon={faEdit} className="action-icon" /></td>
                             <td>
@@ -73,48 +55,7 @@ const ItemList = ({ items, onItemClick, username }) => {
                 </tbody>
             </table>
 
-            {showMessagePopup && (
-                <div>
-                    <div className={classes.transparentBackdrop}></div>
-                    <div className={`${classes.messagePopup} ${classes.popupStyles}`}>
-                        <div className={classes.popUpHeader}>
-                            <h2>Reclamo Número: {selectedItem.id}</h2>
-                            <div className={classes.dataHeader}>
-                                <p><span>Nombre:</span> {selectedItem.name}</p>
-                                <p><span>Patente:</span> {selectedItem.patente}</p>
-                            </div>
-                        </div>
-
-                        <div>
-                            <h3 className={classes.comentTitle}>Comentarios:</h3>
-                            {comments.length > 0 ? (
-                                <ul>
-                                    {comments.map((comment, index) => (
-                                        <li key={index} className={classes.coment}>
-                                            <p><strong>{comment.user}:</strong> {comment.text}</p>
-                                            <p>{comment.date}</p>
-                                        </li>
-                                    ))}
-                                </ul>
-                            ) : (
-                                <p className={classes.noHay}>No hay comentarios aún.</p>
-                            )}
-                        </div>
-                        <form onSubmit={handleCommentSubmit}>
-                            <textarea
-                                placeholder="Escribe tu comentario aquí..."
-                                value={comment}
-                                onChange={handleCommentChange}
-                                className={classes.commentTextArea}
-                            />
-                            <button type="submit" className={classes.btnComent}>Enviar</button>
-                        </form>
-                        <div className={classes.popUpFooter}>
-                            <button onClick={handleCloseMessagePopup} className={classes.cerrar}>Cerrar</button>
-                        </div>
-                    </div>
-
-                </div>
+            {showMessagePopup && ( <CommentsPopUp selectedComplaint={selectedItem} setShowCommentsPopUp={setShowCommentsPopUp} username={username}/>
             )}
         </div>
     );

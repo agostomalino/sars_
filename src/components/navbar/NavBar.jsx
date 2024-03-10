9f8f2a252cc7d859217f734861c720dd080f44f8
Agostina Tomalino <agostomalino@gmail.com>
2024-03-10 15:42:24 -0300

    Navbar button change its behaivor and text according to route

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,18 +1,49 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import classes from './NavBar.module.css';
 
 const NavBar = () => {
+    const [buttonText, setButtonText] = useState('Iniciar sesión');
+    const [route, setRoute] = useState('/');
+    const location = useLocation();
+
+    useEffect(() => {
+        const currentPath = location.pathname;
+        let newText = '';
+
+        switch (currentPath) {
+            case '/':
+                newText = 'Iniciar sesión';
+                setRoute('/sars_/log-in');
+                break;
+            case '/listado':
+                newText = 'Nuevo Reclamo';
+                setRoute('/formulario');
+                break;
+            default:
+                newText = 'Inicio';
+                setRoute('/');
+                break;
+        }
+
+        setButtonText(newText);
+    }, [location.pathname]);
+
+    const handleButtonClick = () => {
+        // Redirige al usuario a la ruta específica
+        window.location.href = route;
+    }
+
     return (
         <header className={classes.header}>
             <div className={classes.headerLeft}>
                 <Link to="/" className={classes.logo}><h1 className='logo-h'>SACS</h1></Link>
             </div>
             <div className={classes.headerRight}>
-                <Link to="/log-in" className={classes.btn}>Iniciar sesión</Link>
+                <button className={classes.btn} onClick={handleButtonClick}>{buttonText}</button>
             </div>
         </header>
     )
 }
 
-export default NavBar
+export default NavBar;

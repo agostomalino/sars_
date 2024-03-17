65248c5dc38e09fd14dd00ada6172e57929266a0
Agostina Tomalino <agostomalino@gmail.com>
2024-03-16 22:03:14 -0300

    FormSiniestro style

--- src/components/forms/formSiniestro/FormSiniestro.jsx
+++ src/components/forms/formSiniestro/FormSiniestro.jsx
@@ -1,6 +1,5 @@
 import React, {useState} from 'react';
-
-
+import classes from './FormSiniestro.module.css';
 
 const provinciasArgentinas = [
     "Buenos Aires",
@@ -76,130 +75,154 @@ const FormSiniestro = () => {
     };
 
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="fecha">Fecha:</label>
-                    <input
-                        type="text"
-                        id="fecha"
-                        value={fecha}
-                        onChange={(e) => setFecha(e.target.value)}
+        <div className={classes.container}>
+            <form onSubmit={handleSubmit} className={classes.form}>
+                <div className={classes.formGroup}>
+                    <div className={classes.row}>
+                        <div className={classes.col}>
+                            <label htmlFor="fecha" className={classes.label}>Fecha:</label>
+                            <input
+                                type="text"
+                                id="fecha"
+                                value={fecha}
+                                onChange={(e) => setFecha(e.target.value)}
+                                required
+                                className={classes.input}
+                            />
+                        </div>
+                        <div className={classes.col}>
+                            <label htmlFor="hora" className={classes.label}>Hora:</label>
+                            <input
+                                type="time"
+                                id="hora"
+                                value={hora}
+                                onChange={(e) => setHora(e.target.value)}
+                                required
+                                className={classes.input}
+                            />       
+                        </div>
+                    </div>
+                </div>
+                <div className={classes.row}>
+                        <div className={classes.col}>
+                            <label htmlFor="pais" className={classes.label}>País:</label>
+                            <select
+                                id="pais"
+                                value={pais}
+                                onChange={handlePaisChange}
+                                required
+                                className={classes.select}
+                            >
+                                <option value="">Seleccione un país</option>
+                                <option value="argentina">Argentina</option>
+                                <option value="chile">Chile</option>
+                                <option value="brasil">Brasil</option>
+                                <option value="paraguay">Paraguay</option>
+                                <option value="uruguay">Uruguay</option>
+                                <option value="bolivia">Bolivia</option>
+                            </select>
+                        </div>
+                        <div className={classes.col}>
+                            <label htmlFor="provincia" className={classes.label}>Provincia:</label>
+                            <select
+                                id="provincia"
+                                value={provincia}
+                                onChange={handleProvinciaChange}
+                                required
+                                className={classes.select}
+                            >
+                                <option value="provincia">Seleccione una provincia</option>
+                                {provinciasArgentinas.map((provincia, index) => (
+                                    <option key={index} value={provincia}>{provincia}</option>
+                                ))}
+                            </select>
+                        </div>
+                    </div>
+                <div className={classes.formGroup}>
+                    <div className={classes.row}>
+                        <div className={classes.col}>
+                            <label htmlFor="calle" className={classes.label}>Calle:</label>
+                            <input
+                                type="text"
+                                id="calle"
+                                value={calle}
+                                onChange={(e) => setCalle(e.target.value)}
+                                required
+                                className={classes.input}
+                            />
+                        </div>
+                        <div className={classes.col}>
+                            <label htmlFor="altura" className={classes.label}>Altura:</label>
+                            <input
+                                type="text"
+                                id="altura"
+                                value={altura}
+                                onChange={(e) => setAltura(e.target.value)}
+                                required
+                                className={classes.input}
+                            />
+                        </div>
+
+                    </div>
+                </div>
+                <div className={classes.formGroup}>
+                    <div className={classes.row}>
+                        <div className={classes.col}>
+                            <label htmlFor="interseccion" className={classes.label}>Intersección</label>
+                            <input
+                                type="text"
+                                id="interseccion"
+                                value={interseccion}
+                                onChange={(e) => setInterseccion(e.target.value)}
+                                required
+                                className={classes.input}
+                            />
+                        </div>
+                        <div className={classes.col}>
+                            <label htmlFor="entre" className={classes.label}>Entre calles</label>
+                            <input
+                                type="text"
+                                id="entre"
+                                value={entre}
+                                onChange={(e) => setEntre(e.target.value)}
+                                required
+                                className={classes.input}
+                            />
+                        </div>
+                    </div>
+                </div>
+    
+                <div className={classes.formGroup}>
+                    <div className={classes.row}>
+                        <div className={classes.col}>
+                            <label htmlFor="monto" className={classes.label}>Monto Daños</label>
+                            <input
+                                type="text"
+                                id="monto"
+                                value={monto}
+                                onChange={(e) => setMonto(e.target.value)}
+                                required
+                                className={classes.input}
+                            />
+                        </div>
+                        <div className={classes.col}></div>
+                    </div>
+                </div>
+    
+                <div className={classes.formGroup}>
+                    <label htmlFor="relato" className={classes.label}>Relato del hecho</label>
+                    <textarea
+                        id="relato"
+                        value={relato}
+                        onChange={handleRelatoChange}
                         required
+                        className={classes.textarea}
                     />
                 </div>
-                <div>
-                    <label htmlFor="hora">Hora:</label>
-                    <input
-                        type="time"
-                        id="hora"
-                        value={hora}
-                        onChange={(e) => setHora(e.target.value)}
-                        required
-                    />
-                </div>
-                <div>
-                    <label htmlFor="pais">País:</label>
-                    <select
-                        id="pais"
-                        value={pais}
-                        onChange={handlePaisChange}
-                        required
-                    >
-                        <option value="">Seleccione un país</option>
-                        <option value="argentina">Argentina</option>
-                        <option value="chile">Chile</option>
-                        <option value="brasil">Brasil</option>
-                        <option value="paraguay">Paraguay</option>
-                        <option value="uruguay">Uruguay</option>
-                        <option value="bolivia">Bolivia</option>
-                    </select>
-                </div>
-
-                <div>
-                    <label htmlFor="provincia">Provincia:</label>
-                    <select
-                    id="provincia"
-                    value={provincia}
-                    onChange={handleProvinciaChange}
-                    required
-                    >
-                    <option value="">Seleccione una provincia</option>
-                    {provinciasArgentinas.map((provincia, index) => (
-                        <option key={index} value={provincia}>{provincia}</option>
-                    ))}
-                    </select>
-                </div>
-
-                <div>
-                    <label htmlFor="calle">Calle:</label>
-                    <input
-                        type="text"
-                        id="calle"
-                        value={calle}
-                        onChange={(e) => setCalle(e.target.value)}
-                        required
-                    />
-                </div>
-
-                <div>
-                    <label htmlFor="calle">Altura:</label>
-                    <input
-                        type="text"
-                        id="altura"
-                        value={altura}
-                        onChange={(e) => setAltura(e.target.value)}
-                        required
-                    />
-                </div>
-
-                <div>
-                    <label htmlFor="calle">Intersección</label>
-                    <input
-                        type="text"
-                        id="interseccion"
-                        value={interseccion}
-                        onChange={(e) => setInterseccion(e.target.value)}
-                        required
-                    />
-                </div>
-
-                <div>
-                    <label htmlFor="entre">Entre calles</label>
-                    <input
-                        type="text"
-                        id="entre"
-                        value={entre}
-                        onChange={(e) => setEntre(e.target.value)}
-                        required
-                    />
-                </div>
-
-                <div>
-                    <label htmlFor="monto">Monto Daños</label>
-                    <input
-                        type="text"
-                        id="monto"
-                        value={monto}
-                        onChange={(e) => setMonto(e.target.value)}
-                        required
-                    />
-                </div>
-
-                <div>
-                <label htmlFor="relato">Relato del hecho</label>
-                <textarea
-                    id="relato"
-                    value={relato}
-                    onChange={handleRelatoChange}
-                    required
-                />
-                </div>
-
-                <button type="submit">Enviar</button>
+    
+                <button type="submit" className={classes.submitButton}>Enviar</button>
             </form>
         </div>
     );
-};
-
+}
 export default FormSiniestro;

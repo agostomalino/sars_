9756e8cffb4929c89576857ce576ee4bc86b53bb
Agostina Tomalino <agostomalino@gmail.com>
2024-04-02 19:18:39 -0300

    formVehiculo, formReclamo added

--- src/components/forms/formReclamado/FormReclamado.jsx
+++ src/components/forms/formReclamado/FormReclamado.jsx
@@ -1,9 +1,146 @@
-import React from 'react'
+import React, { useState } from 'react';
+import classes from './FormReclamado.module.css'; 
 
 const FormReclamado = () => {
+    const [titular, setTitular] = useState(false);
+    const [lesionado, setLesionado] = useState(false);
+    const [numeroDocumento, setNumeroDocumento] = useState('');
+    const [nombre, setNombre] = useState('');
+    const [apellido, setApellido] = useState('');
+    const [telefonoFijo, setTelefonoFijo] = useState('');
+    const [telefonoMovil, setTelefonoMovil] = useState('');
+    const [email, setEmail] = useState('');
+    const [esPersonaJuridica, setEsPersonaJuridica] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Enviar los datos del formulario 
+    };
+
     return (
-        <div>FormReclamado</div>
-    )
+        <form onSubmit={handleSubmit} className={classes.container}>
+            <div className={classes.row}>
+                <div className={classes.col}>
+                    <label htmlFor="titular" className={classes.label}>Titular:</label>
+                    <div className={classes.checkboxContainer}>
+                        <label>
+                            <input
+                                type="checkbox"
+                                checked={titular}
+                                onChange={() => setTitular(!titular)}
+                                className={classes.checkbox}
+                            /> 
+                        </label>
+                    </div>
+                </div>
+                <div className={classes.col}>
+                    <label htmlFor="lesionado" className={classes.label}>Lesionado:</label>
+                    <div className={classes.checkboxContainer}>
+                        <label>
+                            <input
+                                type="checkbox"
+                                checked={lesionado}
+                                onChange={() => setLesionado(!lesionado)}
+                                className={classes.checkbox}
+                            />
+                        </label>
+                    </div>
+                </div>
+            </div>
+            <div className={classes.row}>
+                <div className={classes.col}>
+                    <label htmlFor="numeroDocumento" className={classes.label}>Número Documento:</label>
+                    <input
+                        type="text"
+                        id="numeroDocumento"
+                        value={numeroDocumento}
+                        onChange={(e) => setNumeroDocumento(e.target.value)}
+                        className={classes.input}
+                    />
+                </div>
+            </div>
+            <div className={classes.row}>
+                <div className={classes.col}>
+                    <label htmlFor="nombre" className={classes.label}>Nombre:</label>
+                    <input
+                        type="text"
+                        id="nombre"
+                        value={nombre}
+                        onChange={(e) => setNombre(e.target.value)}
+                        className={classes.input}
+                    />
+                </div>
+                <div className={classes.col}>
+                    <label htmlFor="apellido" className={classes.label}>Apellido:</label>
+                    <input
+                        type="text"
+                        id="apellido"
+                        value={apellido}
+                        onChange={(e) => setApellido(e.target.value)}
+                        className={classes.input}
+                    />
+                </div>
+            </div>
+            <div className={classes.row}>
+                <div className={classes.col}>
+                    <label htmlFor="telefonoFijo" className={classes.label}>Teléfono Fijo:</label>
+                    <input
+                        type="text"
+                        id="telefonoFijo"
+                        value={telefonoFijo}
+                        onChange={(e) => setTelefonoFijo(e.target.value)}
+                        className={classes.input}
+                    />
+                </div>
+                <div className={classes.col}>
+                    <label htmlFor="telefonoMovil" className={classes.label}>Teléfono Móvil:</label>
+                    <input
+                        type="text"
+                        id="telefonoMovil"
+                        value={telefonoMovil}
+                        onChange={(e) => setTelefonoMovil(e.target.value)}
+                        className={classes.input}
+                    />
+                </div>
+            </div>
+            <div className={classes.row}>
+                <div className={classes.col}>
+                    <label htmlFor="email" className={classes.label}>Email:</label>
+                    <input
+                        type="email"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        className={classes.input}
+                    />
+                </div>
+            </div>
+            <div className={classes.row}>
+                <div className={classes.col}>
+                    <label className={classes.label}>Es persona jurídica:</label>
+                    <div className={classes.checkboxContainer}>
+                        <label>
+                            <input
+                                type="checkbox"
+                                checked={esPersonaJuridica}
+                                onChange={() => setEsPersonaJuridica(!esPersonaJuridica)}
+                                className={classes.checkbox}
+                            /> SI
+                        </label>
+                        <label>
+                            <input
+                                type="checkbox"
+                                checked={!esPersonaJuridica}
+                                onChange={() => setEsPersonaJuridica(!esPersonaJuridica)}
+                                className={classes.checkbox}
+                            /> NO
+                        </label>
+                    </div>
+                </div>
+            </div>
+            <button type="submit" className={classes.submitButton}>Enviar</button>
+        </form>
+    );
 }
 
-export default FormReclamado
+export default FormReclamado;
--- src/components/forms/formVehiculo/FormVehiculo.jsx
+++ src/components/forms/formVehiculo/FormVehiculo.jsx
@@ -1,58 +1,125 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
+import classes from './FormVehiculo.module.css';
 
 const FormVehiculo = () => {
-    // Estados para cada campo del formulario
-    const [nombre, setNombre] = useState('');
-    const [email, setEmail] = useState('');
-    const [mensaje, setMensaje] = useState('');
+    const [dominio, setDominio] = useState('');
+    const [tipoVehiculo, setTipoVehiculo] = useState('');
+    const [marcaModelo, setMarcaModelo] = useState('');
+    const [ano, setAno] = useState('');
+    const [companiaAseguradora, setCompaniaAseguradora] = useState('');
+    const [destruccionTotal, setDestruccionTotal] = useState(false);
+    const [franquicia, setFranquicia] = useState(false);
 
-    // Función para manejar el envío del formulario
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        // Aca enviar al server
-        console.log('Nombre:', nombre);
-        console.log('Email:', email);
-        console.log('Mensaje:', mensaje);
-        // Limpiar los campos del formulario
-        setNombre('');
-        setEmail('');
-        setMensaje('');
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Enviar los datos del formulario
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-        <div>
-            <label htmlFor="nombre">Nombre:</label>
-            <input
-            type="text"
-            id="nombre"
-            value={nombre}
-            onChange={(e) => setNombre(e.target.value)}
-            required
-            />
-        </div>
-        <div>
-            <label htmlFor="email">Email:</label>
-            <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            />
-        </div>
-        <div>
-            <label htmlFor="mensaje">Mensaje:</label>
-            <textarea
-            id="mensaje"
-            value={mensaje}
-            onChange={(e) => setMensaje(e.target.value)}
-            required
-            />
-        </div>
-        <button type="submit">Enviar</button>
+        <form onSubmit={handleSubmit} className={classes.container}>
+            <div className={classes.row}>
+                <div className={classes.col}>
+                    <label htmlFor="dominio" className={classes.label}>Dominio:</label>
+                    <input
+                        type="text"
+                        id="dominio"
+                        value={dominio}
+                        onChange={(e) => setDominio(e.target.value)}
+                        className={classes.input}
+                    />
+                </div>
+                <div className={classes.col}>
+                    <label htmlFor="tipoVehiculo" className={classes.label}>Tipo de Vehiculo:</label>
+                    <input
+                        type="text"
+                        id="tipoVehiculo"
+                        value={tipoVehiculo}
+                        onChange={(e) => setTipoVehiculo(e.target.value)}
+                        className={classes.input}
+                    />
+                </div>
+            </div>
+            <div className={classes.row}>
+                <div className={classes.col}>
+                    <label htmlFor="marcaModelo" className={classes.label}>Marca y Modelo:</label>
+                    <input
+                        type="text"
+                        id="marcaModelo"
+                        value={marcaModelo}
+                        onChange={(e) => setMarcaModelo(e.target.value)}
+                        className={classes.input}
+                    />
+                </div>
+                <div className={classes.col}>
+                    <label htmlFor="ano" className={classes.label}>Año:</label>
+                    <input
+                        type="text"
+                        id="ano"
+                        value={ano}
+                        onChange={(e) => setAno(e.target.value)}
+                        className={classes.input}
+                    />
+                </div>
+            </div>
+            <div className={classes.row}>
+                <div className={classes.col}>
+                    <label htmlFor="companiaAseguradora" className={classes.label}>Compañia aseguradora:</label>
+                    <input
+                        type="text"
+                        id="companiaAseguradora"
+                        value={companiaAseguradora}
+                        onChange={(e) => setCompaniaAseguradora(e.target.value)}
+                        className={classes.input}
+                    />
+                </div>
+            </div>
+            <div className={classes.row}>
+                <div className={classes.col}>
+                    <label htmlFor="destruccionTotal" className={classes.label}>Tiene cobertura destrucción total:</label>
+                    <div className={classes.checkboxContainer}>
+                        <label>
+                            <input
+                                type="checkbox"
+                                checked={destruccionTotal}
+                                onChange={() => setDestruccionTotal(!destruccionTotal)}
+                                className={classes.checkbox}
+                            /> SI
+                        </label>
+                        <label>
+                            <input
+                                type="checkbox"
+                                checked={!destruccionTotal}
+                                onChange={() => setDestruccionTotal(!destruccionTotal)}
+                                className={classes.checkbox}
+                            /> NO
+                        </label>
+                    </div>
+                </div>
+                <div className={classes.col}>
+                    <label htmlFor="franquicia" className={classes.label}>Tiene franquicia:</label>
+                    <div className={classes.checkboxContainer}>
+                        <label>
+                            <input
+                                type="checkbox"
+                                checked={franquicia}
+                                onChange={() => setFranquicia(!franquicia)}
+                                className={classes.checkbox}
+                            /> SI
+                        </label>
+                        <label>
+                            <input
+                                type="checkbox"
+                                checked={!franquicia}
+                                onChange={() => setFranquicia(!franquicia)}
+                                className={classes.checkbox}
+                            /> NO
+                        </label>
+                    </div>
+                </div>
+            </div>
+            <button type="submit" className={classes.submitButton}>Enviar</button>
         </form>
     );
 }
 
-export default FormVehiculo
+export default FormVehiculo;

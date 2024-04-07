41d0238707d80fda2c33b1b339936d1cd8fc2bbd
Agostina Tomalino <agostomalino@gmail.com>
2024-04-07 19:45:38 -0300

    Breadcrumb icons added

--- src/components/breadcrumb/Breadcrumb.jsx
+++ src/components/breadcrumb/Breadcrumb.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMapMarkerAlt, faUser, faCar } from '@fortawesome/free-solid-svg-icons'; // Importa los íconos necesarios
 import classes from './Breadcrumb.module.css';
 
 const Breadcrumb = ({ items, onItemClick }) => {
+
+    const iconMap = {
+        'Datos del Siniestro': faMapMarkerAlt,
+        'Reclamante': faUser,
+        'Vehiculo del Reclamante': faCar,
+        'Reclamado': faUser,
+        'Vehiculo del Tercero': faCar
+    };
+
     return (
         <nav aria-label="breadcrumb">
             <ol className={classes.breadcrumb}>
                 {items.map((item) => (
                     <li key={item.id} className={classes.breadcrumbItem}>
-                        <Link to="#" onClick={() => onItemClick(item.id)} className={classes.breadcrumbLink}>{item.text}</Link>
+                        <Link to="#" onClick={() => onItemClick(item.id)} className={classes.breadcrumbLink}>
+                            <FontAwesomeIcon icon={iconMap[item.text]} className={classes.breadcrumbIcon} />
+                            {item.text}
+                        </Link>
                     </li>
                 ))}
             </ol>
